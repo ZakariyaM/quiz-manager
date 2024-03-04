2ac959a37c0d5c8c79dcfc456457a8c5c7bab8e7
Zakariya Mohamed <zkariy@amazon.com>
2024-03-04 13:00:47 +0000

    updating API endpoint URL

--- react-quiz-frontend/src/constants.js
+++ react-quiz-frontend/src/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  API_ENDPOINT_BASE_URL: "http://localhost:5002"
+  API_ENDPOINT_BASE_URL: "https://quiz-manager-cyv0n5aio-zakariya-mohameds-projects-6e43c162.vercel.app/:5002"
 };
 
 export default constants;
