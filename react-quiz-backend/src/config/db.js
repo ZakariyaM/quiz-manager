a0fbff34cff4d856f7d4a0d597d38dcc62dc9032
Zakariya Mohamed <zkariy@amazon.com>
2024-03-04 12:55:45 +0000

    Updating database to be hosted on neon

--- react-quiz-backend/src/config/db.js
+++ react-quiz-backend/src/config/db.js
@@ -1,12 +1,30 @@
 const { Pool } = require('pg');
+require("dotenv").config({ path: "./config.env" });
 
-// Create a new PostgreSQL pool instance
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'password',
-  port: 5432 // default PostgreSQL port
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
 });
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT version()');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPgVersion();
+
 
 module.exports = pool;
