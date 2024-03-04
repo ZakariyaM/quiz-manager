5025300589844a9b37e362891def790adc9218f6
Zakariya Mohamed <zkariy@amazon.com>
2024-03-04 13:10:12 +0000

    Updating URL to remove backslash

--- react-quiz-frontend/src/constants.js
+++ react-quiz-frontend/src/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  API_ENDPOINT_BASE_URL: "https://quiz-manager-three.vercel.app/:5002"
+  API_ENDPOINT_BASE_URL: "https://quiz-manager-three.vercel.app:5002"
 };
 
 export default constants;
